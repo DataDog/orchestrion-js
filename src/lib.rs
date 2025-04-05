6976c92373828bd0ebc80dfb5555945b4cf3ad4b
Bryan English <bryan.english@datadoghq.com>
2025-04-05 17:09:48 +0200

    add repo files, and license info in files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2025 Datadog, Inc.
+ **/
+
 //! # Orchestrion
 //! Orchestrion is a library for instrumenting Node.js libraries at build or load time.
 //! It provides [`VisitMut`] implementations for SWC's AST nodes, which can be used to insert
